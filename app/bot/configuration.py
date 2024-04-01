cd1022eab0f421fce7f4c88354b9dbdb87a07ff5
Jamie Caprani <jamiecaprani@gmail.com>
2024-04-01 01:22:50 +0100

    option

--- app/bot/configuration.py
+++ app/bot/configuration.py
@@ -1,18 +1,23 @@
 from binance import Client
+from coinmarketcapapi import CoinMarketCapAPI
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 def init():
+    config = {
+        "coin_market_cap_client": CoinMarketCapAPI(os.getenv('COIN_MARKET_CAP'))
+    }
     if os.getenv('MAINNET') == 1:
         api_key = os.getenv('BINANCE_PUBLIC')
         api_secret = os.getenv('BINANCE_SECRET')
-        return Client(api_key, api_secret)    
+        config["binance_client"] = Client(api_key, api_secret)    
     else:     
         api_key = os.getenv('BINANCE_PUBLIC_TEST')
         api_secret = os.getenv('BINANCE_SECRET_TEST')
         testnet_url = 'https://testnet.binance.vision/api'
         client = Client(api_key, api_secret)
         client.API_URL = testnet_url
-        return client
+        config["binance_client"] = client
+    return config
